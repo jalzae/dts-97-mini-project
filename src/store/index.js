ba47cff6f8b8f4d293fd3fa4fb5bb8dce1d70b0b
jalzae <jalzae@gmail.com>
2022-07-16 08:26:21 +0700

    Update Folder Structure

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// import counterSlice from "./service/movies.js";
+
+export const store = configureStore({
+
+ reducer: {
+  // counterRTK: counterSlice,
+ },
+});
