7e8da01b431626cf0e165680ef3209921b5ef5e9
grarizki <28901232+grarizki@users.noreply.github.com>
2022-07-20 19:27:48 +0700

    setup movies in row

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
+import Row from './components/Row';
+import requests from './requests.js'
 
 function App() {
 
 
   return (
     <div className="App">
-      <Row title="Movies" />
-      <Row title="Trending Now" />
+      <Row title="Movies" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
