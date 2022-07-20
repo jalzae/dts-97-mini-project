d0a9ad28997db7d67cd8005849ad06264f58a67e
grarizki <28901232+grarizki@users.noreply.github.com>
2022-07-20 19:32:29 +0700

    change image size

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../api/axios';
 
-const base_url = "https://image.tmdb.org/t/p/original/"
+const base_url = "https://image.tmdb.org/t/p/w500/"
 
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
